fb441540a9cefee604061044edd0363c28c546d7
arnaudderison <arnaud.derison@hotmail.com>
2024-12-15 16:53:51 +0100

    init app

---
+++ logtime19/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      coucou dans app
+    </>
+  )
+}
+
+export default App
---
+++ logtime19/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Logtime19',
+        short_name: 'Logtime',
+        description: 'Application pour consulter les logtimes de l’école 19',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+})
